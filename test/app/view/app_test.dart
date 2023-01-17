51e06dac693e7a1fe422731a9c0d33d2e3675941
Jake Hadley <jakehadley10@gmail.com>
2023-01-16 21:38:04 -0700

    linter fix

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -5,10 +5,10 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
-import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:four_gospels/app/app.dart';
-import 'package:four_gospels/home/home.dart';
+// import 'package:four_gospels/app/app.dart';
+// import 'package:four_gospels/home/home.dart';
 
 void main() {
   group('App', () {
